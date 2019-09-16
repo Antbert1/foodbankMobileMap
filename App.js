4e8a0e790cf1dfa9a7a190f26c9ba161684bf7e0
Anthea <anthea4002@gmail.com>
2019-09-16 21:36:37 +0100

    Basic places api request

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableWithoutFeedback } from 'react-native';
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as Actions from "./actions";
@@ -7,12 +7,34 @@ import MapComponent from './src/components/MapComponent';
 
 
 class App extends Component {
-  render() {
+  buttonPressed() {
     debugger;
+    let params = "key=AIzaSyD5HV9mXQMenEP_nfvo_jd2tYfXZV1kkEM&input=Food%20Bank&inputtype=textquery";
+    var parameters = {
+      key: "AIzaSyD5HV9mXQMenEP_nfvo_jd2tYfXZV1kkEM",
+      input: "Food Bank",
+      inputtype: "textquery"
+    };
+    fetch("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?" + params)
+    .then(response => response.json())
+			.then(jsonResponse => {
+        debugger;
+				console.log(jsonResponse);
+			})
+    .catch(error=>console.log(error)) //to catch the errors if any
+
+  }
+
+  render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Services</Text>
         <MapComponent />
+        <View>
+        <TouchableWithoutFeedback onPress={() => this.buttonPressed()}>
+          <Text>Tap</Text>
+        </TouchableWithoutFeedback>
+        </View>
       </View>
     );
   }
--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -4,7 +4,6 @@ import MapView from 'react-native-maps';
 
 const styles = StyleSheet.create({
     container: {
-      ...StyleSheet.absoluteFillObject,
       height: 400,
       width: 400,
       justifyContent: 'flex-end',
